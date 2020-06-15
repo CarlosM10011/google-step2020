db787fda92f8e5abeab71025185f81ac4e001a56
Carlos Medrano <crlsmedrano99@gmail.com>
2020-06-15 16:13:08 -0500

    Fix typo in AuthStatus.java

--- portfolio/src/main/java/com/google/sps/data/AuthStatus.java
+++ portfolio/src/main/java/com/google/sps/data/AuthStatus.java
@@ -14,7 +14,7 @@
 
 package com.google.sps.data;
 
-/** Immutable class that holds user info */
+/** Immutable class that holds user info. */
 public final class AuthStatus {
 
   private final String email;
