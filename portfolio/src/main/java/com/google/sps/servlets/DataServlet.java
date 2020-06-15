72eae502452ed0d11472efc293edb5c45ee877fa
Carlos Medrano <crlsmedrano99@gmail.com>
2020-06-15 18:06:35 -0500

    Migrate constants in DataServlet.java to constants file

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -31,54 +31,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Servlet that returns all comment data. */
-@WebServlet("/data")
+@WebServlet(Constants.DATA_SERVLET_PATH)
 public class DataServlet extends HttpServlet {
 
-  private final String COMMENT_ENTITY_NAME = "Comment";
   private final ArrayList<Comment> comments = new ArrayList<Comment>();
   private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-  private final String GET_CONTENT_TYPE = "text/json;";
   private final Gson gson = new Gson();
-  private final String POST_MESSAGE_PARAMETER = "body";
-  private final String POST_NAME_PARAMETER = "name";
-  private final String POST_REDIRECT_URL = "/";
-  private final String PROPERTY_COMMENT_CREATED_NAME = "created";
-  private final String PROPERTY_COMMENT_MESSAGE_NAME = "message";
-  private final String PROPERTY_COMMENT_NAME = "name";
   private final Query query =
-      new Query(this.COMMENT_ENTITY_NAME)
-          .addSort(this.PROPERTY_COMMENT_CREATED_NAME, SortDirection.DESCENDING);
+      new Query(Constants.COMMENT_ENTITY_NAME)
+          .addSort(Constants.PROPERTY_COMMENT_CREATED_NAME, SortDirection.DESCENDING);
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     this.comments.clear();
     PreparedQuery results = this.datastore.prepare(this.query);
     for (Entity comment : results.asIterable()) {
-      final String name = (String) comment.getProperty(this.PROPERTY_COMMENT_NAME);
-      final Date created = new Date((long) comment.getProperty(this.PROPERTY_COMMENT_CREATED_NAME));
-      final String message = (String) comment.getProperty(this.PROPERTY_COMMENT_MESSAGE_NAME);
+      final String name = (String) comment.getProperty(Constants.PROPERTY_COMMENT_NAME);
+      final Date created =
+          new Date((long) comment.getProperty(Constants.PROPERTY_COMMENT_CREATED_NAME));
+      final String message = (String) comment.getProperty(Constants.PROPERTY_COMMENT_MESSAGE_NAME);
       this.comments.add(new Comment(name, created, message));
     }
     String output = gson.toJson(this.comments);
-    response.setContentType(this.GET_CONTENT_TYPE);
+    response.setContentType(Constants.JSON_GET_CONTENT_TYPE);
     response.getWriter().println(output);
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    final String name = request.getParameter(POST_NAME_PARAMETER);
-    final String body = request.getParameter(POST_MESSAGE_PARAMETER);
+    final String name = request.getParameter(Constants.COMMENT_POST_NAME_PARAMETER);
+    final String body = request.getParameter(Constants.COMMENT_POST_MESSAGE_PARAMETER);
     if (!isSaneForm(name, body)) {
       response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid form input.");
       return;
     }
     final long created = System.currentTimeMillis();
-    final Entity commentEntity = new Entity(this.COMMENT_ENTITY_NAME);
-    commentEntity.setProperty(this.PROPERTY_COMMENT_NAME, name);
-    commentEntity.setProperty(this.PROPERTY_COMMENT_CREATED_NAME, created);
-    commentEntity.setProperty(this.PROPERTY_COMMENT_MESSAGE_NAME, body);
+    final Entity commentEntity = new Entity(Constants.COMMENT_ENTITY_NAME);
+    commentEntity.setProperty(Constants.PROPERTY_COMMENT_NAME, name);
+    commentEntity.setProperty(Constants.PROPERTY_COMMENT_CREATED_NAME, created);
+    commentEntity.setProperty(Constants.PROPERTY_COMMENT_MESSAGE_NAME, body);
     this.datastore.put(commentEntity);
-    response.sendRedirect(this.POST_REDIRECT_URL);
+    response.sendRedirect(Constants.DEFAULT_POST_REDIRECT_URL);
   }
 
   /** Checks to see if the comment form sent via the html post is valid. */
